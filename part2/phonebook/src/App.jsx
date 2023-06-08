ccf98ad960866a101e62692efd18ba0af829e52c
Semih Orhan <sorhan3.14@gmail.com>
2023-06-08 09:40:28 +0300

    finish exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,7 +8,12 @@ const App = () => {
 
   const handleSubmit = e => {
     e.preventDefault()
-    setPersons(persons.concat({name: newName}))
+    const personIsExists = persons.find(person => person.name === newName)
+    
+    personIsExists ? 
+      alert(`${newName} is already added to phonebook`) :
+      setPersons(persons.concat({name: newName}))
+    setNewName('')
   }
 
   return (
