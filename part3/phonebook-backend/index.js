943cd21ff3d8c0bc72897c94102f216958237bb4
Semih Orhan <sorhan3.14@gmail.com>
2023-06-13 14:39:31 +0300

    finish exercise 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,6 +4,14 @@ import express from 'express'
 const PORT = 3001
 const app = express()
 
+app.get('/info', (req, res) => {
+    res.send(
+        `<p>
+            Phone has info for ${persons.length} people <br/>
+            ${new Date().toUTCString()}
+        </p>`)
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
